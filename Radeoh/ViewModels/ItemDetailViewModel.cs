5af9d3f59f34aba708072b5aa58a405a828fe7f3
Martin Juul Christiansen <mjc@evercall.dk>
2020-10-17 08:49:12 +0200

    initial commit

---
+++ Radeoh/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Radeoh.Models;
+
+namespace Radeoh.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
