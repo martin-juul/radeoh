b879cc6fff8f41f9e5910ae05b95b9af3af50cb4
Martin Juul <code@juul.xyz>
2020-10-20 21:13:48 +0200

    WIP: player implementation

--- Radeoh/Views/MainPage.xaml.cs
+++ Radeoh/Views/MainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NLog;
+using Radeoh.Models;
+using Radeoh.Support;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,7 +15,8 @@ namespace Radeoh.Views
     public partial class MainPage : ContentPage
     {
         ViewModels.MainPageViewModel _viewModel = new ViewModels.MainPageViewModel();
-        
+        private readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
         public MainPage()
         {
             InitializeComponent();
@@ -24,6 +27,21 @@ namespace Radeoh.Views
         {
             base.OnAppearing();
             _viewModel.FetchStationsCommand.Execute(null);
+        }
+
+        async void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            
+            if (e.GetType() == typeof(Station))
+            {
+                Station station = e.Item as Station;
+                await Navigation.PushAsync(new Player(station));
+            }
+            else
+            {
+                e.Json();
+                _logger.Error("Wrong item", e.Item);
+            }
         }
     }
 }
--- Radeoh/Views/Player.xaml.cs
+++ Radeoh/Views/Player.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Acr.UserDialogs;
+using Radeoh.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,39 @@ namespace Radeoh.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Player : ContentPage
     {
-        public Player()
+        public Station Station;
+        
+        public Player(Station station)
         {
             InitializeComponent();
+        }
+        
+        void OnPlayPauseButtonClicked(object sender, EventArgs args)
+        {
+            switch (MediaElement.CurrentState)
+            {
+                case MediaElementState.Closed:
+                case MediaElementState.Stopped:
+                case MediaElementState.Paused:
+                    MediaElement.Play();
+                    break;
+                case MediaElementState.Playing:
+                    MediaElement.Pause();
+                    break;
+                case MediaElementState.Opening:
+                    UserDialogs.Instance.Loading("Loading");
+                    break;
+                case MediaElementState.Buffering:
+                    UserDialogs.Instance.Loading("Buffering");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        void OnStopButtonClicked(object sender, EventArgs args)
+        {
+            MediaElement.Stop();
         }
     }
 }
